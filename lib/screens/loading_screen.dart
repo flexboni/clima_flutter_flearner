39dd6f64aeaed372d0907f058da089670c07aef0
flexboni <kgb8594@gmail.com>
2021-11-09 13:46:02 +0900

    get current position with geolocator package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLoaction() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLoaction();
           },
           child: const Text('Get Location'),
         ),
